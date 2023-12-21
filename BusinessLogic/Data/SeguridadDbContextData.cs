12a259a68e678949dfb2efde74b2c89e2e88ca87
Daniel Salazar <dsnile.salazr@gmail.com>
2023-12-21 15:44:35 -0600

    Creacion de Usuario con IdentityCore Manager

---
+++ BusinessLogic/Data/SeguridadDbContextData.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Data
+{
+    public class SeguridadDbContextData
+    {
+
+        public static async Task SeedUserAsync(UserManager<Usuario> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var usuario = new Usuario()
+                {
+                    Nombre = "Daniel",
+                    Apellido = "Salazar",
+                    UserName = "Dasa",
+                    Direccion = new Direccion()
+                    {
+                        Calle = "Foresta 131",
+                        Ciudad = "Monterrey",
+                        CodigoPostal = "282929",
+                        Departamento = "Mty",
+                    }
+                };
+                await userManager.CreateAsync(usuario, "12345678");
+
+            }
+        }
+    }
+}

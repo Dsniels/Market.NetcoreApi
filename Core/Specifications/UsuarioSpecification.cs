aee1309c1c8400832a04fcb658049fc0de073b33
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-08 00:39:22 -0600

    Specificaciones para ordenamiento de usuario

---
+++ Core/Specifications/UsuarioSpecification.cs
@@ -0,0 +1,42 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class UsuarioSpecification : BaseSpecification<Usuario>
+    {
+        public UsuarioSpecification(UsuarioSpecificationParams usuarioParams) 
+        : base(x => 
+            ( string.IsNullOrEmpty(usuarioParams.Search) || x.Nombre.Contains(usuarioParams.Search)) &&
+            (string.IsNullOrEmpty(usuarioParams.Nombre) || x.Nombre.Contains(usuarioParams.Nombre)) &&
+            (string.IsNullOrEmpty(usuarioParams.Apellido) || x.Apellido.Contains(usuarioParams.Apellido))
+        )
+        { 
+            ApplyPaging(usuarioParams.PageSize * (usuarioParams.PageIndex - 1), usuarioParams.PageSize);
+
+            if (!string.IsNullOrEmpty(usuarioParams.Sort))
+            {
+                switch(usuarioParams.Sort)
+                {
+                    case "nombreAsc":
+                        AddOrderBy(p => p.Nombre); break;
+                    case "nombreDesc":
+                        AddOrderByDescending(p => p.Nombre); break;
+                    case "emailAsc":
+                        AddOrderBy(p => p.Email); break;
+                    case "emailDesc":
+                        AddOrderByDescending(p => p.Email); break;
+                    default:
+                        AddOrderBy(p => p.Nombre); break;
+                }
+            }
+
+
+
+        }
+    }
+}

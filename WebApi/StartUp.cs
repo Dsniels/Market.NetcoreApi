571ca18e161cbc1f9a0754ffd12bd4b142d1dc90
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-08 00:43:27 -0600

    Configuracion de servicio GenericSeguridadRepository en startup

--- WebApi/StartUp.cs
+++ WebApi/StartUp.cs
@@ -58,6 +58,7 @@ namespace WebApi
 
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped(typeof(IGenericSeguridadRepository<>), typeof(GenericSeguridadRepository<>));
             services.AddDbContext<MarketDbContext>(opt =>
             {
                 opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));

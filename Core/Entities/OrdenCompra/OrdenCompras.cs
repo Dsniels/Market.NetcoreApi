559ca9550de5b8c495c4edf5659326878f0b56ff
Daniel Salazar <dsnile.salazr@gmail.com>
2024-03-23 16:11:43 -0600

    Entidad OrdenCompra

---
+++ Core/Entities/OrdenCompra/OrdenCompras.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrdenCompra
+{
+    public class OrdenCompras : ClaseBase
+    {
+        public OrdenCompras()
+        {
+        }
+
+        public OrdenCompras(string compradorEmail, Direccion direccionEnvio, TipoEnvio tipoEnvio, IReadOnlyList<OrderItem> orderItems, decimal subTotal)
+        {
+            CompradorEmail = compradorEmail;
+            DireccionEnvio = direccionEnvio;
+            TipoEnvio = tipoEnvio;
+            OrderItems = orderItems;
+            SubTotal = subTotal;
+        }
+
+        public string CompradorEmail { get; set; }
+        public DateTimeOffset OrdenCompraFecha { get; set; } = DateTimeOffset.Now;
+        public Direccion DireccionEnvio { get; set; }
+        public TipoEnvio TipoEnvio { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal SubTotal { get; set; }
+
+        public OrdenStatus OrdenStatus { get; set; } = OrdenStatus.Pendiente;
+
+        public string PagoIntentoId { get; set; }
+
+        public decimal GetTotal()
+        {
+            return SubTotal + TipoEnvio.Precio;
+        }
+
+    }
+}

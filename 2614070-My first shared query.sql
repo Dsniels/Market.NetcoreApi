a90c8d3b077632e2ef7bafc15d3a69782c6bd9ee
Daniel Salazar <dsnile.salazr@gmail.com>
2024-05-23 02:18:51 +0000

    Update query `My first shared query`

--- 2614070-My first shared query.sql
+++ 2614070-My first shared query.sql
@@ -1,1 +1,7 @@
-CREATE TABLE Productos
+CREATE TABLE Estudiantes (
+   ID INT PRIMARY KEY,
+   Nombre VARCHAR(50) NOT NULL,
+   Apellido VARCHAR(50) NOT NULL,
+   Edad INT,
+   CorreoElectronico VARCHAR(100)
+);
